0661ce54e4ec1f06abad64abee46d45f8494c150
Kevin Ortega <kevin.ortega2011@gmail.com>
2025-10-02 16:12:19 -0400

    Update favicon from .ico to .png format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,11 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   themeColor: "#6b21a8",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
   openGraph: {
     type: "website",
     locale: "en_US",
